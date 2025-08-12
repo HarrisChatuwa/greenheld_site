6634222cdd4d7159fe6c66c88089540cfbb97bb3
Harris Chatuwa <hchatuwa@gmail.com>
2025-08-12 23:00:26 +0200

    better icons in admin

--- admin/projects.php
+++ admin/projects.php
@@ -54,18 +54,13 @@ if (isset($_SESSION['success_message'])) {
                         <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm truncate max-w-xs"><?php echo htmlspecialchars($project['description']); ?></td>
                         <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm"><img src="../<?php echo htmlspecialchars($project['image_url']); ?>" alt="<?php echo htmlspecialchars($project['title']); ?>" width="100"></td>
                         <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm truncate max-w-xs"><?php echo htmlspecialchars($project['outcome']); ?></td>
-                        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm whitespace-nowrap">
-                            <div class="flex items-center space-x-4">
-                                <button data-modal-target="#edit-project-modal-<?php echo $project['id']; ?>" class="text-indigo-600 hover:text-indigo-900">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block" viewBox="0 0 20 20" fill="currentColor">
-                                        <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
-                                        <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
-                                    </svg>
+                        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm whitespace-nowrap text-center">
+                            <div class="flex item-center justify-center space-x-4">
+                                <button data-modal-target="#edit-project-modal-<?php echo $project['id']; ?>" class="w-6 h-6 text-gray-500 hover:text-indigo-600 focus:outline-none" title="Edit">
+                                    <svg xmlns="http://www.w.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" /></svg>
                                 </button>
-                                <a href="delete_project.php?id=<?php echo $project['id']; ?>" class="text-red-600 hover:text-red-900" onclick="return confirm('Are you sure?')">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block" viewBox="0 0 20 20" fill="currentColor">
-                                        <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm4 0a1 1 0 012 0v6a1 1 0 11-2 0V8z" clip-rule="evenodd" />
-                                    </svg>
+                                <a href="delete_project.php?id=<?php echo $project['id']; ?>" class="w-6 h-6 text-gray-500 hover:text-red-600 focus:outline-none" title="Delete" onclick="return confirm('Are you sure?')">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" /></svg>
                                 </a>
                             </div>
                         </td>
